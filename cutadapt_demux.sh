830a8880a4a9098aa8eb2947b93cc22bdf86451c
Andres Aranda-Diaz <andres.arandadiaz@gmail.com>
2022-01-31 23:30:06 -0800

    modified .sh files to use _quarter files which I created by zcatting and then head -n 20000000 on the original fastq files

--- cutadapt_demux.sh
+++ cutadapt_demux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir results/cutadapt_TES_demux
+mkdir results/cutadapt_TES_quarter_demux
 
 cutadapt \
     --action=trim \
@@ -10,7 +10,7 @@ cutadapt \
     --pair-adapters \
     -e 0 \
     --no-indels \
-    -o results/cutadapt_TES_demux/{name}_trimmed_R1.fastq.gz \
-    -p results/cutadapt_TES_demux/{name}_trimmed_R2.fastq.gz \
-    data/raw_fastq/TES_R1_001.fastq.gz \
-    data/raw_fastq/TES_R2_001.fastq.gz 
+    -o results/cutadapt_TES_quarter_demux/{name}_trimmed_R1.fastq.gz \
+    -p results/cutadapt_TES_quarter_demux/{name}_trimmed_R2.fastq.gz \
+    data/raw_fastq/TES_quarter_R1_001.fastq.gz \
+    data/raw_fastq/TES_quarter_R2_001.fastq.gz 
