f0605e4ed6a01ef3ce96145bdee07900b5a23fe7
Brian Palmer <brian.palmer@ucsf.edu>
2023-03-05 13:44:22 -0800

    add preprocesing table to check allele counts

--- R_code/postdada_rearrange.R
+++ R_code/postdada_rearrange.R
@@ -23,6 +23,45 @@ library(BSgenome)
 library(tidyr)
 library(doMC)
 
+
+## Postprocessing QC
+
+seqtab.nochim <- readRDS(args$dada2_output)
+seqtab.nochim.df = as.data.frame(seqtab.nochim)
+seqtab.nochim.df$sample = rownames(seqtab.nochim)
+seqtab.nochim.df[seqtab.nochim.df==0]=NA
+pat="-1A_|-1B_|-1_|-2_|-1AB_|-1B2_"
+seqtab.nochim.df = seqtab.nochim.df %>% 
+  pivot_longer(cols = seq(1,ncol(seqtab.nochim)),names_to = "asv",values_to = "reads",values_drop_na=TRUE) %>% 
+  mutate(locus = paste0(sapply(strsplit(sample,"_"),"[",1),"_",sapply(strsplit(sample,"_"),"[",2),"_",sapply(strsplit(sample,"_"),"[",3)))%>% 
+  mutate(sampleID = sapply(strsplit(sapply(strsplit(sample,pat),"[",2),"_trimmed"),"[",1)) %>% 
+  select(sampleID,locus,asv,reads)
+
+temp = seqtab.nochim.df %>% select(locus,asv) %>% distinct()
+loci =unique(temp$locus)
+k=1
+allele.sequences = data.frame(locus = seq(1,nrow(temp)),allele = seq(1,nrow(temp)),sequence = seq(1,nrow(temp)))
+for(i in seq(1,length(loci))){
+  temp2 = temp %>% filter(locus==loci[i]) 
+  for(j in seq(1,nrow(temp2))){
+    allele.sequences$locus[k+j-1] = loci[i]
+    allele.sequences$allele[k+j-1] = paste0(loci[i],".",j)
+    allele.sequences$sequence[k+j-1] = temp2$asv[j]
+  }
+  k=k+nrow(temp2)
+}
+
+allele.data = seqtab.nochim.df %>% 
+  left_join(allele.sequences %>% select(-locus),by=c("asv"="sequence")) %>% 
+  group_by(sampleID,locus,allele) %>%
+  mutate(norm.reads.allele = reads/sum(reads))%>% 
+  group_by(sampleID,locus) %>%
+  mutate(norm.reads.locus = reads/sum(reads))%>% 
+  mutate(n.alleles = n())
+
+saveRDS(allele.data,file="pre_processed_allele_table.RDS")
+write.table(allele.data,file="pre_processed_allele_table.txt",quote=F,sep="\t",col.names=T,row.names=F)
+
 seqtab.nochim <- readRDS(args$dada2_output)
 
 ## I. Check for non overlapping sequences.
